b9bd00a8a8cd24ac890b63454652a03d3bcd618f
Andonis <andonisgeorgiou@gmail.comgit config --global user.email andonisgeorgiou@gmail.com>
2016-10-15 12:05:25 +1100

    Updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,51 +22,53 @@ a6 = Artist.create :name => 'Sue Jones', :location => 'Paris', :dob => '1979/3/2
 # ----------------------------------------------------------------------------------------------
 
 Doodle.destroy_all
-d1 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url => 'http://drawingimage.com/files/1/Psychedelic-Art-Photo-Drawing.jpg'
+d1 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url => 'http://drawingimage.com/files/1/Psychedelic-Art-Photo-Drawing.jpg'
 
-d2 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://godfreyl.ism-online.org/files/2014/01/Blobs1.jpg'
-
-
-d3 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://i.ytimg.com/vi/jKPSJrsE2I8/maxresdefault.jpg'
+d2 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://godfreyl.ism-online.org/files/2014/01/Blobs1.jpg'
 
 
-d4 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://s-media-cache-ak0.pinimg.com/236x/28/00/98/2800985317aa814665716a700395741c.jpg'
+d3 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://i.ytimg.com/vi/jKPSJrsE2I8/maxresdefault.jpg'
 
 
-d5 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://i.ytimg.com/vi/e4N882MPTPQ/maxresdefault.jpg'
+d4 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://s-media-cache-ak0.pinimg.com/236x/28/00/98/2800985317aa814665716a700395741c.jpg'
 
 
-d6 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://thumbs.dreamstime.com/x/star-sketchy-notebook-doodles-lined-paper-11011850.jpg'
+d5 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://i.ytimg.com/vi/e4N882MPTPQ/maxresdefault.jpg'
 
 
-d7 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://www.google.com/logos/doodles/2014/doodle-4-google-winner-ireland-6225298102157312-hp2x.jpg'
+d6 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://thumbs.dreamstime.com/x/star-sketchy-notebook-doodles-lined-paper-11011850.jpg'
 
 
-d8 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/doodling_away_psweb.jpg'
+d7 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://www.google.com/logos/doodles/2014/doodle-4-google-winner-ireland-6225298102157312-hp2x.jpg'
 
 
-d9 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://cdn.mos.cms.futurecdn.net/2d58b9faaf30a41afe498c0381d3f801.jpg'
+d8 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/doodling_away_psweb.jpg'
 
 
-d10 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://thumb1.shutterstock.com/display_pic_with_logo/1409680/246778888/stock-vector-valentines-day-kiss-love-doodle-vector-246778888.jpg'
+d9 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://cdn.mos.cms.futurecdn.net/2d58b9faaf30a41afe498c0381d3f801.jpg'
 
 
-d11 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://static.nerdstargamer.net/doodles/gallery/dang-doodle-smug-bug.png'
+d10 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://thumb1.shutterstock.com/display_pic_with_logo/1409680/246778888/stock-vector-valentines-day-kiss-love-doodle-vector-246778888.jpg'
 
 
-d12 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://static.qbrushes.net/downloads/215-2221/images/large/1975-Flower-Doodles.jpg'
+d11 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://static.nerdstargamer.net/doodles/gallery/dang-doodle-smug-bug.png'
 
 
-d13 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'https://thumb9.shutterstock.com/display_pic_with_logo/221395/221395,1295804889,2/stock-vector-love-doodles-69582940.jpg'
+d12 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://static.qbrushes.net/downloads/215-2221/images/large/1975-Flower-Doodles.jpg'
 
 
-d14 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/internal_security_psweb.jpg'
+d13 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'https://thumb9.shutterstock.com/display_pic_with_logo/221395/221395,1295804889,2/stock-vector-love-doodles-69582940.jpg'
 
 
-d15 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/internal_security_psweb.jpg'
+d14 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/internal_security_psweb.jpg'
 
 
-a1.doodles << d1 << d2 << d3
+d15 = Doodle.create :title => 'Psychedelic Vision #1', :caption => 'It seemed so real', :image_url =>'http://cdn.theatlantic.com/static/coma/images/issues/200609/doodles/internal_security_psweb.jpg'
+
+d16 = Doodle.create :title => 'Cloudinary Image #1', :caption => 'It seemed so real', :image_url => 'cgkc8ixrlkhb0j7gz4ft'
+
+
+a1.doodles << d1 << d2 << d3 << d16
 a2.doodles << d4 << d5 << d6
 a3.doodles << d7 << d8 << d9
 a4.doodles << d10 << d11
